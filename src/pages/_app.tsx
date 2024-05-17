4d452ee585875ea737f90c0bf2556652ed07b3a9
Diaz Linggaputra <diazlinggaputra@gmail.com>
2024-05-17 08:38:19 +0700

    feat: initiate chakra ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,38 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import {
+  ChakraBaseProvider,
+  extendTheme,
+  type ThemeConfig,
+  // ColorModeScript,
+  theme as base
+} from '@chakra-ui/react'
+import { CSSObject } from '@emotion/react'
 
+const config: ThemeConfig = {
+  initialColorMode: 'light',
+  useSystemColorMode: false
+}
+
+const fonts = {
+  heading: `Roboto Mono, ${base.fonts.heading}`
+}
+
+const theme = extendTheme({
+  styles: {
+    global: (props: CSSObject) => ({
+      body: {
+        bg: props.colorMode === 'dark' ? '#0f172a' : 'white'
+      }
+    })
+  },
+  config,
+  fonts
+})
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraBaseProvider theme={theme}>
       <Component {...pageProps} />;
-    </ChakraProvider>
+    </ChakraBaseProvider>
   )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,28 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="description"
+          content="Maximum productivity: Achieve more, work less, and enjoy life!"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
