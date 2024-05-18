d15be3bab681d89d94379df85ace1664f73dc753
Diaz Linggaputra <diazlinggaputra@gmail.com>
2024-05-18 09:11:11 +0700

    feat: implement main application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,38 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import {
-  ChakraBaseProvider,
-  extendTheme,
-  type ThemeConfig,
-  // ColorModeScript,
-  theme as base
-} from '@chakra-ui/react'
-import { CSSObject } from '@emotion/react'
 
-const config: ThemeConfig = {
-  initialColorMode: 'light',
-  useSystemColorMode: false
-}
-
-const fonts = {
-  heading: `Roboto Mono, ${base.fonts.heading}`
-}
-
-const theme = extendTheme({
-  styles: {
-    global: (props: CSSObject) => ({
-      body: {
-        bg: props.colorMode === 'dark' ? '#0f172a' : 'white'
-      }
-    })
-  },
-  config,
-  fonts
-})
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraBaseProvider theme={theme}>
-      <Component {...pageProps} />;
-    </ChakraBaseProvider>
-  )
+  return <Component {...pageProps} />
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,128 @@
-import Image from 'next/image'
+import moment from 'moment'
+import { FaPrint, FaPlus, FaTimes } from 'react-icons/fa'
+import { useState, useRef, Fragment } from 'react'
 import { Inter } from 'next/font/google'
+import { useReactToPrint } from 'react-to-print'
+import Head from 'next/head'
 
-const inter = Inter({ subsets: ['latin'] })
+const TodoComponent = () => (
+  <tr className="font-semibold">
+    <td className="py-2">
+      <select
+        className="outline-none appearance-none cursor-pointer"
+        defaultValue="Meetings"
+      >
+        <option value="Meetings">Meetings</option>
+        <option value="Focused">Focused</option>
+        <option value="Meals/Breaks">Meals/Breaks</option>
+        <option value="Unfocused">Unfocused</option>
+        <option value="Focused">Focused</option>
+        <option value="Close">Close</option>
+      </select>
+    </td>
+    <td>
+      <input type="time" className="outline-none" />
+    </td>
+    <td>
+      <input
+        type="text"
+        placeholder="Specify your task"
+        className="outline-none"
+      />
+    </td>
+    <td>
+      <input
+        type="number"
+        defaultValue={15}
+        min="1"
+        max="480"
+        className="outline-none"
+      />
+    </td>
+    <td>
+      <input
+        type="checkbox"
+        className="cursor-pointer text-black"
+        defaultChecked
+      />
+    </td>
+  </tr>
+)
 
 export default function Home() {
+  const [todoList, setTodoList] = useState<JSX.Element[]>([<TodoComponent />])
+
+  const handleAddTodoList = () => {
+    setTodoList([...todoList, <TodoComponent />])
+  }
+
+  const handleResetTodoList = () => {
+    setTodoList([<TodoComponent />])
+  }
+
+  const supadayTodoRef = useRef<any>()
+
+  const handlePrintPreview = useReactToPrint({
+    content: () => supadayTodoRef.current
+  })
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      ok mantap
-    </main>
+    <Fragment>
+      <Head>
+        <title>Supaday Todo App</title>
+        <meta
+          name="author"
+          content="FastStartup Developer <diaz@faststartup.io>"
+        />
+      </Head>
+      <main
+        className={`flex flex-col items-center text-black mt-4`}
+        ref={supadayTodoRef}
+      >
+        <div className="flex flex-col gap-5 w-full">
+          <h2 className="text-4xl font-bold italic text-center">
+            {moment().format('dddd MMMM D, YYYY').toUpperCase()}
+          </h2>
+          <h1 className="text-8xl font-bold text-center">SUPADAY</h1>
+          <input
+            className="text-center outline-none uppercase font-semibold italic text-xl"
+            defaultValue={
+              'MAXIMUM PRODUCTIVITY: ACHIEVE MORE, WORK LESS, AND ENJOY LIFE!'
+            }
+          />
+
+          <table className="text-left w-[75%] mx-auto">
+            <thead>
+              <tr>
+                <th>Time Habit</th>
+                <th>Start Time</th>
+                <th>Tasks to Do During This Time</th>
+                <th># of Minutes</th>
+                <th>Do it</th>
+              </tr>
+            </thead>
+            <tbody>{todoList}</tbody>
+          </table>
+        </div>
+      </main>
+      <div
+        onClick={handleResetTodoList}
+        className="fixed bottom-36 right-4 p-3 bg-red-500 text-white rounded-full shadow-lg cursor-pointer hover:bg-red-600"
+      >
+        <FaTimes size={24} />
+      </div>
+      <div
+        onClick={handleAddTodoList}
+        className="fixed bottom-20 right-4 p-3 bg-green-500 text-white rounded-full shadow-lg cursor-pointer hover:bg-green-600"
+      >
+        <FaPlus size={24} />
+      </div>
+      <div
+        onClick={handlePrintPreview}
+        className="fixed bottom-4 right-4 p-3 bg-blue-500 text-white rounded-full shadow-lg cursor-pointer hover:bg-blue-600"
+      >
+        <FaPrint size={24} />
+      </div>
+    </Fragment>
   )
 }
